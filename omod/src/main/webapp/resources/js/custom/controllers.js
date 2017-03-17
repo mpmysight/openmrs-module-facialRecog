edf0d6a99c0b57abf3e6a273e4eeb12d48bba437
Sight <mpmysight@gmail.com>
2017-03-17 15:37:09 +0300

    saving photo from the video stream

--- omod/src/main/webapp/resources/js/custom/controllers.js
+++ omod/src/main/webapp/resources/js/custom/controllers.js
@@ -26,7 +26,9 @@ function videoError(e) {
 function CaptureCtrl ($scope, $location) {
 
       var video = document.getElementById('video');
+      var captureButton = document.getElementById('captureButton');
       var canvas = document.getElementById('canvas');
+      var canvas_capture = document.getElementById('canvas_capture');
       var context = canvas.getContext('2d');
       var tracker = new tracking.ObjectTracker('face');
       tracker.setInitialScale(4);
@@ -49,4 +51,42 @@ function CaptureCtrl ($scope, $location) {
       gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
       gui.add(tracker, 'stepSize', 1, 5).step(0.1);
 
+      //trying new code
+      video.addEventListener('canplay', function(ev) {
+          if (!streaming) {
+            height = video.videoHeight / (video.videoWidth / width);
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
+          }
+        }, false);
+
+        captureButton.addEventListener('click', function(ev) {
+          	if(captureButton.innerText==="Capture")
+            {
+            	takepicture();
+            }
+            else
+            {
+            	video.style.display = "block";
+            	canvas_capture.style.display = "none";
+              captureButton.innerText= "Capture";
+            }
+            ev.preventDefault();
+          }, false);
 }
+
+function takepicture() {
+  	//video.style.display = "none";
+    canvas_capture.style.display = "block";
+    captureButton.innerText= "Retake";
+  	canvas_capture.width = 200;
+    canvas_capture.height = 150;
+    canvas_capture.getContext('2d').drawImage(video, 0, 0, canvas_capture.width, canvas_capture.height);
+    //canvas_capture.getContext('2d').drawImage(video, rect.x, rect.y, rect.width, rect.height, rect.x, rect.y, rect.width, rect.height);
+    var data = canvas_capture.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
