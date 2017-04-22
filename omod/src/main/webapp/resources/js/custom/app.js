255bbdce92b0e1729f22addf800d47dbfd6e8951
mssavai <mssavai@gmail.com>
2017-04-22 18:05:03 +0300

    Add logic to upload image data

--- omod/src/main/webapp/resources/js/custom/app.js
+++ omod/src/main/webapp/resources/js/custom/app.js
@@ -5,13 +5,15 @@ facialrecog.
         $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file):/);
         $routeProvider.when('/captureImage', {controller: CaptureCtrl,
             templateUrl: '../../moduleResources/facialrecog/partials/captureImage.html'});
-
-
-      /*  $routeProvider.when('/cohortdefinition', {controller: CohortDefinitionCtrl,
-            templateUrl: '../../moduleResources/facialrecog/partials/cohortdefinition.html'});
-        $routeProvider.when('/cohortdefinition/:uuid', {controller: CohortDefinitionCtrl,
-            templateUrl: '../../moduleResources/facialrecog/partials/cohortdefinition.html'});
-        $routeProvider.when('/createcohortdefinition', {controller: CohortDefinitionCtrl,
-            templateUrl: '../../moduleResources/facialrecog/partials/cohortdefinition.html'});*/
         $routeProvider.otherwise({redirectTo: '/captureImage'});
     }]);
+
+facialrecog.factory('$data', function ($http) {
+    var identifyFace = function(faceImageData){
+        console.log("Uploading data: "+faceImageData);
+        var $return =  $http.post("identify.json", {"faceimagedata":faceImageData});
+        console.log("Identification Response: "+ JSON.stringify($return));
+    }
+
+    return {identifyFace:identifyFace}
+});
